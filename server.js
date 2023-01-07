7afc01160672875845e43f3aed2bb055ddfb42ca
mdinkelbach <mj.dinkelbach@mgail.com>
2023-01-07 13:50:54 -0600

    inquirer1

--- server.js
+++ server.js
@@ -8,34 +8,39 @@ const db = mysql.createConnection(
       password: 'ufrK$ys-Je4~~:~,Ns0N6HEkB',
       database: 'employee_db'
     },
-    console.log(`Connected to the employee_db database.`)
   );
 
-  inquirer
-  .prompt([
-    {
-      type: 'list',
-      name: 'option',
-      message: 'What would you like to do?',
-      choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee', 'Exit'],
-    },
-  ])
-  .then((data) => {
-    if (data.option === 'View all Departments') {
-        console.log('departments')
-    } else if (data.option === 'View all Roles') {
-        console.log('roles')
-    } else if (data.option === 'View all Employees') {
-        console.log('employees')
-    } else if (data.option === 'Add a Department') {
-        console.log('add department')
-    } else if (data.option === 'Add a Role') {
-        console.log('add role')
-    } else if (data.option === 'Add a Employee') {
-        console.log('add employee')
-    } else if (data.option === 'Update an Employee') {
-        console.log('update employee')
-    } else {
-        console.log('leave')
-    }
-  });
+inquirer
+.prompt([
+   {
+     type: 'list',
+     name: 'option',
+     message: 'What would you like to do?',
+     choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee', 'Exit'],
+   },
+])
+ .then((data) => {
+  if (data.option === 'View all Departments') {
+      db.query('SELECT name, id FROM department', function (err, results) {
+          console.log(results);
+        });
+  } else if (data.option === 'View all Roles') {
+      db.query('SELECT role.title, role.id AS role_id, department.name AS department, salary FROM role JOIN department ON role.department_id = department.id;', function (err, results) {
+          console.log(results);
+        });
+  } else if (data.option === 'View all Employees') {
+      db.query('SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;', function (err, results) {
+          console.log(results);
+        });
+  } else if (data.option === 'Add a Department') {
+      console.log('add department')
+  } else if (data.option === 'Add a Role') {
+      console.log('add role')
+  } else if (data.option === 'Add a Employee') {
+      console.log('add employee')
+  } else if (data.option === 'Update an Employee') {
+      console.log('update employee')
+  } else {
+      console.log('leave')
+  }
+});
