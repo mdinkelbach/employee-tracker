b24770fdcb8079570c9cd15cbdaeaadfdef517ea
mdinkelbach <mj.dinkelbach@mgail.com>
2023-01-12 13:48:48 -0600

    managers

--- server.js
+++ server.js
@@ -23,23 +23,11 @@ inquirer
 ])
  .then((data) => {
   if (data.option === 'View all Departments') {
-      db.query('SELECT name, id FROM department', function (err, results) {
-        const table = cTable.getTable(results)
-        console.log(table)
-        start()
-        });
+    viewDepartments()
   } else if (data.option === 'View all Roles') {
-      db.query('SELECT role.title, role.id AS role_id, department.name AS department, salary FROM role JOIN department ON role.department_id = department.id;', function (err, results) {
-        const table = cTable.getTable(results)
-        console.log(table)
-        start()
-        });
+    viewRoles()
   } else if (data.option === 'View all Employees') {
-      db.query('SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id;', function (err, results) {
-        const table = cTable.getTable(results)
-        console.log(table)
-        start()
-        });
+    viewEmployees()
   } else if (data.option === 'Add a Department') {
     addDept()
   } else if (data.option === 'Add a Role') {
@@ -55,6 +43,52 @@ inquirer
 });
 }
 
+function viewDepartments() {
+  db.query('SELECT name, id FROM department', function (err, results) {
+    console.log('\n');
+        console.table("Departments", results);
+        console.log('\n');
+        });
+        setTimeout(() => {
+            start();
+        }, 500);
+}
+
+function viewRoles() {
+  db.query('SELECT role.title, role.id AS role_id, department.name AS department, salary FROM role JOIN department ON role.department_id = department.id;', function (err, results) {
+        console.log('\n');
+        console.table("Roles", results);
+        console.log('\n');
+        });
+        setTimeout(() => {
+            start();
+        }, 500);
+}
+
+function viewEmployees() {
+        let empList = [];
+        let employees = [];
+        db.query('SELECT first_name, last_name FROM employee ORDER BY id', function (err, empResults) {
+        employees = empResults;
+        let firstAndLast = [];
+        employees.forEach(element => {
+            firstAndLast.push(`${element.first_name} ${element.last_name}`);
+        });
+        db.query('SELECT employee.id AS employee_id, employee.first_name, employee.last_name, role.title AS title, department.name AS department, role.salary, employee.manager_id AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee_id;', function (err, results) {
+            empList = results;
+            empList.forEach(element =>{
+                element.manager = firstAndLast[element.manager-1];
+            })
+            console.log('\n');
+            console.table("Employees", results);
+            console.log('\n');
+          })
+        setTimeout(() => {
+            start();
+        }, 500);
+      })
+    }
+
 function addDept() {
   inquirer
 .prompt([
@@ -68,7 +102,9 @@ function addDept() {
   db.query(`INSERT INTO department (name) VALUES ("${data.department}") `, function (err, results) {
     console.log(`${data.department} added to list of departments.`);
     if (data) {
-      start()
+      setTimeout(() => {
+            start();
+        }, 500);
     }
   });
  });
@@ -97,7 +133,9 @@ function addRole() {
   db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${data.role}", ${data.salary}, ${data.deptid})`, function (err, results) {
     console.log(`${data.role} added to list of roles.`);
     if (data) {
-      start()
+      setTimeout(() => {
+            start();
+        }, 500);
     }
   });
  });
@@ -131,7 +169,9 @@ inquirer
   db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${data.first}", "${data.last}", ${data.roleid}, ${data.manaid})`, function (err, results) {
     console.log(`${data.first} ${data.last} has been added as an employee.`);
     if (data) {
-      start()
+      setTimeout(() => {
+            start();
+        }, 500);
     }
   });
  });
@@ -155,7 +195,9 @@ function updateEmp() {
   db.query(`UPDATE employee SET role_id = ${data.roleid} WHERE id = ${data.empid}`, function (err, results) {
     console.log(`Updated role for Employee ID:${data.empid}.`);
     if (data) {
-      start()
+      setTimeout(() => {
+            start();
+        }, 500);
     }
   });
  });
